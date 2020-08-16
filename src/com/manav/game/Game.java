3c9906726b40c1d53d9f8107f1be01dde0b772ed
Manav Malik <manavmalik36@gmail.com>
2020-08-16 19:36:54 -0400

    made initial commit

---
+++ src/com/manav/game/Game.java
@@ -0,0 +1,115 @@
+package com.manav.game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 160;
+    public static final int HEIGHT = WIDTH / 12 * 9; // game area is 12 x 9
+    public static final float SCALE = 3.0f;
+    public static final String NAME = "Game";
+    private static final long serialVersionUID = 1L;
+    public boolean running = false;
+    public int tickCount = 0;
+    private JFrame frame;
+    private BufferedImage img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+
+    public Game() {
+        setMinimumSize(new Dimension((int) (WIDTH * SCALE), (int) (HEIGHT * SCALE)));
+        setMaximumSize(new Dimension((int) (WIDTH * SCALE), (int) (HEIGHT * SCALE)));
+        setPreferredSize(new Dimension((int) (WIDTH * SCALE), (int) (HEIGHT * SCALE)));
+
+        frame = new JFrame(NAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(this, BorderLayout.CENTER);
+        frame.pack();
+
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Game().start();
+    }
+
+    public synchronized void start() {
+        running = true;
+        new Thread(this).start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        long prevTime = System.nanoTime();
+        double nsPerTick = 1000000000 / 60d;
+
+        int frames = 0;
+        int ticks = 0;
+
+        long lastTimer = System.currentTimeMillis();
+        double unprocessedNS = 0;
+        while (running) {
+            long currTime = System.nanoTime();
+            unprocessedNS += (currTime - prevTime) / nsPerTick;
+            prevTime = currTime;
+            boolean render = true;
+
+            while (unprocessedNS >= 1) {
+                ticks++;
+                tick();
+                unprocessedNS--;
+                render = true;
+            }
+
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (render) {
+                frames++;
+                render();
+            }
+
+            if (System.currentTimeMillis() - lastTimer >= 1000) {
+                lastTimer += 1000;
+                System.out.println(frames + " | " + ticks);
+                frames = 0;
+                ticks = 0;
+            }
+        }
+    }
+
+    public void tick() {
+        tickCount++;
+
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = i * tickCount;
+        }
+    }
+
+    public void render() {
+        BufferStrategy bufferStrategy = getBufferStrategy();
+        if (bufferStrategy == null) {
+            createBufferStrategy(1);
+            return;
+        }
+
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+        graphics.drawImage(img, 0, 0, getWidth(), getHeight(), null);
+        graphics.dispose();
+        bufferStrategy.show();
+    }
+}
